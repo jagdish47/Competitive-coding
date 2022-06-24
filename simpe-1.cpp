38dc94f8684e0f24c3c786ec9b45f0a1df21902f
Jagdish Kumawat <jagdishkumawat81@gmail.com>
2022-06-24 19:03:22 +0530

    first commit

---
+++ simpe-1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    string fname, lname;
+
+    cout<<"Enter your name : ";
+    cin>>fname;
+
+    cout<<"Entery your last name : ";
+    cin>>lname;
+
+    cout<<"Your name is " + fname + " " + lname;
+
+    return 0;
+}
