d0b460850fde58654c8b458222ca7408dd2de218
Jagdish Kumawat <jagdishkumawat81@gmail.com>
2022-06-25 20:52:08 +0530

    sort

--- sort.cpp
+++ sort.cpp
@@ -3,7 +3,6 @@
 // #include <algorithm>
 // using namespace std;
 
-
 // int main(){
 //     int arr[] = {6, 4, 6, 8, 9, 2, 0, 4, -1};
 //     int arrSize = *(&arr + 1) - arr;
@@ -19,22 +18,47 @@
 
 
 //shotcut way to sort an array in descending order.
-#include <iostream>
-#include <algorithm>
+// #include <iostream>
+// #include <algorithm>
+// using namespace std;
+
+// int main(){
+
+//     int arr[] = {6, 4, 6, 8, 9, 2, 0, 4, -1};
+//     int arrSize = *(&arr + 1) - arr;
+//     sort(arr, arr+arrSize);
+//     reverse(arr, arr+arrSize);
+
+//     for(auto e : arr){
+
+//         cout<<e <<" ";
+//     }
+
+//     return 0;
+// }
+
+
+
+
+
+
+#include<iostream>
+#include<algorithm>
 using namespace std;
 
+bool cmp(int a, int b){
+    // return a > b; //descending
+    return a < b; //ascending
+}
 
 int main(){
 
     int arr[] = {6, 4, 6, 8, 9, 2, 0, 4, -1};
     int arrSize = *(&arr + 1) - arr;
-    sort(arr, arr+arrSize);
-    reverse(arr, arr+arrSize);
+
+    sort(arr, arr+arrSize, cmp);
 
     for(auto e : arr){
-
-        cout<<e <<" ";
+        cout<<e<<" ";
     }
-
-    return 0;
 }
